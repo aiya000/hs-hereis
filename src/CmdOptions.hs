9981538585cd5993d54dcee3f246292d1a5697de
aiya000 <aiya000.develop@gmail.com>
2016-07-17 21:30:43 +0900

    Add optList to CmdOptions.options

--- src/CmdOptions.hs
+++ src/CmdOptions.hs
@@ -11,20 +11,25 @@ import System.Console.CmdArgs
 
 -- | hereis options
 data Options = Options
-  { optCd :: Maybe String
+  { optCd   :: Maybe String
+  , optList :: Maybe Bool
   } deriving (Show, Data, Typeable)
 
 
 hereisSummary :: String
 hereisSummary = "hereis is directory changing utility.\n" ++
-                "You can easily bookmarking directory path for change directory."
+                "You can easily bookmarking directory path for change directory.\n" ++
+                "hereis [ --cd name ] [ --list ]"
 
 -- | Options definition
 options :: Options
 options = Options
-  { optCd = Nothing &= explicit
-         &= name "cd"
-         &= help "change directory to target name"
+  { optCd   = Nothing &= explicit
+           &= name "cd"
+           &= help "Change directory to target name"
+  , optList = False &= explicit
+           &= name "list"
+           &= help "Show registered name list"
   }
   &= program "hereis"
   &= summary hereisSummary
