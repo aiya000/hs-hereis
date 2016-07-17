747ddbdd6d2656af3f197dff6df68b6d7a8a6aa4
aiya000 <aiya000.develop@gmail.com>
2016-07-17 21:34:39 +0900

    Add functions behavior

---
+++ src/Control/Hereis.hs
@@ -0,0 +1,8 @@
+-- | Module for app's actions
+module Control.Hereis
+  ( registerPlace
+  ) where
+
+
+registerPlace :: String -> IO ()
+registerPlace placeName = undefined
---
+++ src/Lib.hs
@@ -0,0 +1,8 @@
+-- | Module for library modules exporting
+module Lib
+  ( module CmdOptions
+  , module Control.Hereis
+  ) where
+
+import CmdOptions
+import Control.Hereis
