65b9ae25e17216fabbf57b3c07be8d30dcc42f64
aiya000 <aiya000.develop@gmail.com>
2016-12-07 20:28:36 +0900

    Move 'app' function from executable to library

--- app/Main.hs
+++ app/Main.hs
@@ -1,51 +1,7 @@
-{-# LANGUAGE ScopedTypeVariables #-}
--- | Module for main
 module Main where
 
-import Control.Exception (catch, SomeException)
-import Lib
-import System.Console.CmdArgs (cmdArgs)
+import Data.Hereis.Main (app)
 import System.Environment (getArgs)
 
-type Argument = String
-
-
--- | Program's entry point
 main :: IO ()
-main = do
-  args <- getArgs
-  app args
-
-
--- |
--- Parse the argument, and to do actions
---
--- >>> app []
--- Nothing to do
--- >>> app ["place-name"]
--- registered current directory to the name 'place-name'
--- >>> app ["--cd", "place-name"]
--- changed directory to 'place-name'
--- >>> app ["undefined value"]
--- Unknowned arguments: ["undefined value"]
-app :: [Argument] -> IO ()
--- If cannot get the argument
-app [] = putStrLn "Nothing to do"
-
--- If get the option "--cd"
-app ["--cd", placeName] = undefined
-
--- If get the option "--list"
-app ["--list"] = undefined
-
--- If didn't specify option, register current directory path and name
-app [placeName] = do
-  -- if placeName already exists, show warn message and exit
-  registerPlace placeName `catch` \(e :: SomeException) -> do
-    putStr $ "adding path is failed.\n" ++
-             "hereis detected an error: "
-    fail (show e)
-  putStrLn $ "registered current directory to the name '" ++ placeName ++ "'"
-
--- If get the undefined arguments
-app xs = putStrLn $ "Unknowned arguments: " ++ (show xs)
+main = getArgs >>= app
---
+++ src/Data/Hereis/Main.hs
@@ -0,0 +1,46 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
+-- | Module for library modules exporting
+module Data.Hereis.Main ( app )  where
+
+import CmdOptions
+import Control.Exception (catch, SomeException)
+import Control.Hereis
+import Control.HereisAdd
+import System.Console.CmdArgs (cmdArgs)
+
+type Argument = String
+
+
+-- |
+-- Parse the argument, and to do actions
+--
+-- >>> app []
+-- Nothing to do
+-- >>> app ["place-name"]
+-- registered current directory to the name 'place-name'
+-- >>> app ["--cd", "place-name"]
+-- changed directory to 'place-name'
+-- >>> app ["undefined value"]
+-- Unknowned arguments: ["undefined value"]
+app :: [Argument] -> IO ()
+-- If cannot get the argument
+app [] = putStrLn "Nothing to do"
+
+-- If get the option "--cd"
+app ["--cd", placeName] = undefined
+
+-- If get the option "--list"
+app ["--list"] = undefined
+
+-- If didn't specify option, register current directory path and name
+app [placeName] = do
+  -- if placeName already exists, show warn message and exit
+  registerPlace placeName `catch` \(e :: SomeException) -> do
+    putStr $ "adding path is failed.\n" ++
+             "hereis detected an error: "
+    fail (show e)
+  putStrLn $ "registered current directory to the name '" ++ placeName ++ "'"
+
+-- If get the undefined arguments
+app xs = putStrLn $ "Unknowned arguments: " ++ (show xs)
